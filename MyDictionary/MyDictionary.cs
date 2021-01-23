08216eeaa52603b63b8e93f1fec2f634ec6e42ef
hmtshbz8@gmail.com <hmtshbz8@gmail.com>
2021-01-23 12:33:35 +0300

    Add project files.

---
+++ MyDictionary/MyDictionary.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class MyDictionary<TKey, Tvalue>
+    {
+        TKey[] keys;
+        Tvalue[] values;
+
+
+        public MyDictionary()
+        {
+
+            keys = new TKey[0];
+            values = new Tvalue[0];
+        }
+
+        public void Add(TKey key, Tvalue value)
+        {
+            TKey[] tempKeys = keys;
+            Tvalue[] tempValues = values;
+
+            keys = new TKey[keys.Length + 1];
+            values = new Tvalue[values.Length + 1];
+
+            for (int i = 0; i < tempKeys.Length; i++)
+            {
+                keys[i] = tempKeys[i];
+                values[i] = tempValues[i];
+            }
+
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+
+        }
+
+       
+
+        public int Length
+        {
+            get { return keys.Length; }
+            
+        }
+       
+
+        public TKey[] Keys
+        {
+            get { return keys; }
+            
+        }
+        
+
+        public Tvalue[] Values
+        {
+            get { return values; }
+           
+        }
+
+
+    }
+}
---
+++ MyDictionary/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDictionary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary <int,string> city = new MyDictionary<int,string>();
+
+            city.Add(01, "Adana");
+            city.Add(02, "Adıyaman");
+            city.Add(31, "Hatay");
+            city.Add(32, "Isparta");
+            city.Add(33, "Mersin");
+
+            foreach (var name in city.Keys)
+            {
+                Console.WriteLine(name);
+            }
+            foreach (var number in city.Values)
+            {
+                Console.WriteLine(number);
+
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
